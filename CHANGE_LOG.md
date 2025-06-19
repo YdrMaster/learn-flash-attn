da1bc32ecb81d20474ac226614602a4ff1e6fff1
YdrMaster <ydrml@hotmail.com>
2025-06-19 18:53:22 +0800

    feat: 支持多头注意力
    Signed-off-by: YdrMaster <ydrml@hotmail.com>

--- CHANGE_LOG.md
+++ CHANGE_LOG.md
@@ -7,6 +7,10 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+### Changed
+
+- 支持多头注意力；
+
 ## [0.0.4] - 2025-06-19
 
 ### Added
--- src/attention.rs
+++ src/attention.rs
@@ -21,43 +21,52 @@ pub fn flash_attention(
     assert_eq!(v.dt(), types::F64);
     assert_eq!(o.dt(), types::F64);
 
-    destruct!([n_q, d_q] = q.shape());
-    destruct!([n_o, d_o] = o.shape());
-    destruct!([seq_k, d_k] = k.shape());
-    destruct!([seq_v, d_v] = v.shape());
+    destruct!([h_q, n_q, d_q] = q.shape());
+    destruct!([h_o, n_o, d_o] = o.shape());
+    destruct!([h_k, seq_k, d_k] = k.shape());
+    destruct!([h_v, seq_v, d_v] = v.shape());
 
+    let h = distinct(&[h_q, h_o, h_k, h_v]).unwrap();
     let n = distinct(&[n_q, n_o]).unwrap();
     let s = distinct(&[seq_k, seq_v]).unwrap();
     let d = distinct(&[d_q, d_k, d_v, d_o]).unwrap();
 
     let scale = (d as f64).sqrt().recip();
-    // 处理每个 token
-    for i in 0..n {
-        let q = array::<f64>(q.as_deref().transform(|layout| layout.index(0, i)));
-        let o = array_mut::<f64>(o.as_deref_mut().transform(|layout| layout.index(0, i)));
+    // 处理每个头
+    for i in 0..h {
+        let q = q.as_deref().transform(|layout| layout.index(0, i));
+        let k = k.as_deref().transform(|layout| layout.index(0, i));
+        let v = v.as_deref().transform(|layout| layout.index(0, i));
+        let mut o = o.as_deref_mut().transform(|layout| layout.index(0, i));
 
-        o.fill(0.);
+        // 处理每个 token
+        for i in 0..n {
+            let q = array::<f64>(q.as_deref().transform(|layout| layout.index(0, i)));
+            let o = array_mut::<f64>(o.as_deref_mut().transform(|layout| layout.index(0, i)));
 
-        let len = s - n + i + 1;
-        let mut s = S::new(&[]);
-        for i in 0..len {
-            let k = array::<f64>(k.as_deref().transform(|layout| layout.index(0, i)));
-            let v = array::<f64>(v.as_deref().transform(|layout| layout.index(0, i)));
-            // score = q @ k^T / √d
-            let score = zip(q, k).map(|(q, k)| q * k).sum::<f64>() * scale;
+            o.fill(0.);
 
-            use std::cmp::Ordering::{Equal, Greater, Less};
-            let sum = match s.max.total_cmp(&score) {
-                Equal => [s.sum_exp, 1.],
-                Greater => [s.sum_exp, (score - s.max).exp()],
-                Less => [s.sum_exp * (replace(&mut s.max, score) - score).exp(), 1.],
-            };
+            let len = s - n + i + 1;
+            let mut s = S::new(&[]);
+            for i in 0..len {
+                let k = array::<f64>(k.as_deref().transform(|layout| layout.index(0, i)));
+                let v = array::<f64>(v.as_deref().transform(|layout| layout.index(0, i)));
+                // score = q @ k^T / √d
+                let score = zip(q, k).map(|(q, k)| q * k).sum::<f64>() * scale;
 
-            s.sum_exp = sum[0] + sum[1];
+                use std::cmp::Ordering::{Equal, Greater, Less};
+                let sum = match s.max.total_cmp(&score) {
+                    Equal => [s.sum_exp, 1.],
+                    Greater => [s.sum_exp, (score - s.max).exp()],
+                    Less => [s.sum_exp * (replace(&mut s.max, score) - score).exp(), 1.],
+                };
 
-            let scale = sum.map(|x| x / s.sum_exp);
-            for (v, o) in zip(v, &mut *o) {
-                *o = *o * scale[0] + *v * scale[1]
+                s.sum_exp = sum[0] + sum[1];
+
+                let scale = sum.map(|x| x / s.sum_exp);
+                for (v, o) in zip(v, &mut *o) {
+                    *o = *o * scale[0] + *v * scale[1]
+                }
             }
         }
     }
@@ -109,25 +118,26 @@ mod test {
 
     #[test]
     fn test_flash_attention() {
+        const H: usize = 8;
         const N: usize = 7;
         const S: usize = 2048;
         const D: usize = 256;
 
-        let q = Tensor::from_dim_slice(types::F64, &[N, D]);
-        let k = Tensor::from_dim_slice(types::F64, &[S, D]);
-        let v = Tensor::from_dim_slice(types::F64, &[S, D]);
-        let o = Tensor::from_dim_slice(types::F64, &[N, D]);
+        let q = Tensor::from_dim_slice(types::F64, &[H, N, D]);
+        let k = Tensor::from_dim_slice(types::F64, &[H, S, D]);
+        let v = Tensor::from_dim_slice(types::F64, &[H, S, D]);
+        let o = Tensor::from_dim_slice(types::F64, &[H, N, D]);
 
-        let q_data = random_data(N * D);
-        let k_data = random_data(S * D);
-        let v_data = random_data(S * D);
+        let q_data = random_data(H * N * D);
+        let k_data = random_data(H * S * D);
+        let v_data = random_data(H * S * D);
 
         let q = q.map(|_| erase_ty(&q_data));
         let k = k.map(|_| erase_ty(&k_data));
         let v = v.map(|_| erase_ty(&v_data));
 
         // 计算标准 attention
-        let mut ans = vec![0.0f64; N * D];
+        let mut ans = vec![0.0f64; H * N * D];
         attention(
             q.as_deref(),
             k.as_deref(),
@@ -136,7 +146,7 @@ mod test {
         );
 
         // 计算 flash attention
-        let mut res = vec![0.0f64; N * D];
+        let mut res = vec![0.0f64; H * N * D];
         flash_attention(q, k, v, o.map(|_| erase_ty_mut(&mut res)));
 
         for (ans, res) in zip(ans, res) {
@@ -159,7 +169,7 @@ mod test {
         unsafe { std::slice::from_raw_parts_mut(data.as_mut_ptr().cast(), size_of_val(data)) }
     }
 
-    /// 单个头的基本注意力计算
+    /// 多头注意力计算
     pub fn attention(
         q: Tensor<&[u8]>,
         k: Tensor<&[u8]>,
@@ -171,36 +181,45 @@ mod test {
         assert_eq!(v.dt(), types::F64);
         assert_eq!(o.dt(), types::F64);
 
-        destruct!([n_q, d_q] = q.shape());
-        destruct!([n_o, d_o] = o.shape());
-        destruct!([seq_k, d_k] = k.shape());
-        destruct!([seq_v, d_v] = v.shape());
+        destruct!([h_q, n_q, d_q] = q.shape());
+        destruct!([h_o, n_o, d_o] = o.shape());
+        destruct!([h_k, seq_k, d_k] = k.shape());
+        destruct!([h_v, seq_v, d_v] = v.shape());
 
+        let h = distinct(&[h_q, h_o, h_k, h_v]).unwrap();
         let n = distinct(&[n_q, n_o]).unwrap();
         let s = distinct(&[seq_k, seq_v]).unwrap();
         let d = distinct(&[d_q, d_k, d_v, d_o]).unwrap();
 
         let scale = (d as f64).sqrt().recip();
         let mut score = vec![0.; s];
-        // 处理每个 token
-        for i in 0..n {
-            let q = array::<f64>(q.as_deref().transform(|layout| layout.index(0, i)));
-            let o = array_mut::<f64>(o.as_deref_mut().transform(|layout| layout.index(0, i)));
-            // score = q @ k^T / √d
-            let len = s - n + i + 1;
-            for i in 0..len {
-                let k = array::<f64>(k.as_deref().transform(|layout| layout.index(0, i)));
-                score[i] = zip(q, k).map(|(q, k)| q * k).sum::<f64>() * scale
-            }
-            // causal softmax
-            safe_softmax(&mut score[..len]);
-            score[len..].fill(0.);
-            // o = a @ v // 乘法不连续
-            o.fill(0.);
-            for i in 0..len {
-                let v = array::<f64>(v.as_deref().transform(|layout| layout.index(0, i)));
-                for (v, o) in zip(v, &mut *o) {
-                    *o += score[i] * v
+        // 处理每个头
+        for i in 0..h {
+            let q = q.as_deref().transform(|layout| layout.index(0, i));
+            let k = k.as_deref().transform(|layout| layout.index(0, i));
+            let v = v.as_deref().transform(|layout| layout.index(0, i));
+            let mut o = o.as_deref_mut().transform(|layout| layout.index(0, i));
+
+            // 处理每个 token
+            for i in 0..n {
+                let q = array::<f64>(q.as_deref().transform(|layout| layout.index(0, i)));
+                let o = array_mut::<f64>(o.as_deref_mut().transform(|layout| layout.index(0, i)));
+                // score = q @ k^T / √d
+                let len = s - n + i + 1;
+                for i in 0..len {
+                    let k = array::<f64>(k.as_deref().transform(|layout| layout.index(0, i)));
+                    score[i] = zip(q, k).map(|(q, k)| q * k).sum::<f64>() * scale
+                }
+                // causal softmax
+                safe_softmax(&mut score[..len]);
+                score[len..].fill(0.);
+                // o = a @ v // 乘法不连续
+                o.fill(0.);
+                for i in 0..len {
+                    let v = array::<f64>(v.as_deref().transform(|layout| layout.index(0, i)));
+                    for (v, o) in zip(v, &mut *o) {
+                        *o += score[i] * v
+                    }
                 }
             }
         }
